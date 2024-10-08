a92d622c739021e14c21384390070d7c6d533310
pat_mic <pat_mic@hotmail.com>
2024-10-08 00:49:10 -0500

    se solvento el incidente

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, pat_mic!");
-        System.out.println("Hello, pat_mic...!");
+        System.out.println("hi, pat_mic!");
+        System.out.println("hi, version en pro...!");
     }
 }
